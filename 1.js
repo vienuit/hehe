92c282391725b8197fca89b7cd53d922d1e08ca3
vienuit <48973041+vienuit@users.noreply.github.com>
2020-01-05 11:52:10 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+﻿ $(function(){
+	khoimonan = $('.nhieumon').isotope({
+	  // options
+	  itemSelector: '.motmon',
+	  layoutMode: 'masonry'
+	});
+
+
+	 $('.tieudect a').click(function() {
+	 	/* Act on the event */
+
+	 	dulieu = $(this).data('monan');
+	 	console.log(dulieu);
+	 	khoimonan.isotope({filter:dulieu})
+	 	return false;
+	 });
+
+})  
+ 
